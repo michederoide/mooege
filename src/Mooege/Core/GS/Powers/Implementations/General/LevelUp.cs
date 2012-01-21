c226b9974ac50a4c5997d5c732708aed473c10f5
Wetwlly <xmikecampx@aol.com>
2012-01-21 08:49:50 -0800

    Fixing up barbarian, also some concept design for runes. Added LevelUp spell, needs to be implemented.

---
+++ src/Mooege/Core/GS/Powers/Implementations/General/LevelUp.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Copyright (C) 2011 mooege project
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mooege.Core.GS.Actors;
+using Mooege.Core.GS.Common.Types.Math;
+using Mooege.Net.GS.Message.Definitions.ACD;
+using Mooege.Core.GS.Ticker;
+using Mooege.Core.GS.Powers.Payloads;
+using Mooege.Core.GS.Actors.Movement;
+using Mooege.Core.GS.Common.Types.TagMap;
+using Mooege.Net.GS.Message;
+using Mooege.Core.GS.Players;
+using Mooege.Net.GS.Message.Definitions.Actor;
+
+namespace Mooege.Core.GS.Powers.Implementations
+{
+    [ImplementsPowerSNO(85954)] //g_levelup.pow
+    public class LevelUpBlast : Skill
+    {
+        public override IEnumerable<TickTimer> Main()
+        {
+            WeaponDamage(GetEnemiesInRadius(User.Position, ScriptFormula(2)), ScriptFormula(0), DamageType.Physical);
+            yield break;
+        }
+    }
+}
