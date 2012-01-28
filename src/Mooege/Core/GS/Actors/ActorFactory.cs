0429e2fba22ca636e94cf691e2661b2d21911696
unknown <Demonic@Demonic-PC.(none)>
2012-01-27 21:24:56 -0800

    Adding in BossPortals to dungeons.

--- src/Mooege/Core/GS/Actors/ActorFactory.cs
+++ src/Mooege/Core/GS/Actors/ActorFactory.cs
@@ -84,7 +84,8 @@ namespace Mooege.Core.GS.Actors
                                 return new Portal(world, snoId, tags);
                             case GizmoGroup.BossPortal:
                                 Logger.Warn("Skipping loading of boss portals");
-                                return null;
+                                return new Portal(world, snoId, tags);
+                                //return null;
                             case GizmoGroup.CheckPoint:
                                 return new Checkpoint(world, snoId, tags);
                             case GizmoGroup.Waypoint:
