17be793ca9d2ef7a9baa08f90047fce7a2ab693b
unknown <Demonic@Demonic-PC.(none)>
2012-02-03 16:56:41 -0800

    Cleaned up 151087 and added animations to first 3 zombieskinny

--- src/Mooege/Core/GS/QuestEvents/Implementations/151087.cs
+++ src/Mooege/Core/GS/QuestEvents/Implementations/151087.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Mooege.Common.MPQ.FileFormats;
 using Mooege.Net.GS.Message.Definitions.ACD;
+using Mooege.Net.GS.Message.Definitions.Animation;
 using Mooege.Core.GS.Common.Types.Math;
 using Mooege.Core.GS.Generators;
 
@@ -19,195 +20,247 @@ namespace Mooege.Core.GS.QuestEvents.Implementations
 
         public override void Execute(Map.World world)
         {
-            /*ZombieSkinny's Spawn
-                ActorID: 0x796400CA  ZombieSkinny_Custom_A.acr (2036596938) //this is for zombieskinny #1
-                 Field1: 0x00000003 (3)
-                 Field2: 0
-                 tAnim:
-                 {
-                  PlayAnimationMessageSpec:
-                  {
-                   Duration: 0x00000083 (131 ticks)
-                   AnimationSNO: 0x00002D03:zombie_male_skinny_spawn.ani
-                   PermutationIndex: 0x00000000 (0)
-                   Speed: 1.938229
-                  }
-
             #region ZombieSkinny_Custom_A #1
             //ActorID: 0x796400CA
             //Script code comes here
             //Scale 1.35
             var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354E3);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.7063466f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.7078662f);
-            PRTransform.Vector3D = new Vector3D(2846.162f, 2962.202f, 24.10213f);
+            var actor = actorSNOHandle.Target as Actor; 
+            var PRTransform = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.7063466f,
+                    Vector3D = new Vector3D(0, 0, -0.7078662f)
+                },
+                Vector3D = new Vector3D(2846.162f, 2962.202f, 24.10213f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354E3), PRTransform, world, actor.TagMap);
+            var actorID = WorldGenerator.loadActor(actorSNOHandle, PRTransform, world, actor.TagMap);
 
-            
-
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 3.37346f;
-            // Speed.Value: 0.108
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011070;
-            translateMessage.Position = new Vector3D(2844.901f, 2961.904f, 24.24138f);
             //Send message to all players
-
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                    {
+                        Angle = 3.37346f,
+                        ActorId = (int)actorID,
+                        AnimationTag = 0x00011070,
+                        Position = new Vector3D(2844.901f, 2961.904f, 24.24138f)
+                    }, player.Value);
+
+                world.BroadcastIfRevealed(new PlayAnimationMessage
+                {
+                    ActorID = actorID,
+                    Field1 = 3,
+                    Field2 = 0,
+                    tAnim = new Net.GS.Message.Fields.PlayAnimationMessageSpec[]
+                {
+                    new Net.GS.Message.Fields.PlayAnimationMessageSpec()
+                    {
+                        Duration = 0x00000083,
+                        AnimationSNO = 0x00002D03,
+                        PermutationIndex = 0x00000000,
+                        Speed = 1.938229f
+                    }
+                }
+
+                }, player.Value);
             }
-            //There are no other normals
-            //world.GetActorByDynamicId(actorID).Move(new Vector3D(2850.241f, 2972.256f, 24.69642f), 0);
             #endregion
             #region ZombieSkinny_Custom_A #2
             //Script code comes here
             //ActorID: 0x796500B7  
             //Scale 1.35
-            var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354E3);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.6130235f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.7900648f);
-            PRTransform.Vector3D = new Vector3D(2847.069f, 2975.214f, 24.43476f);
+            var actorSNOHandle2 = new Common.Types.SNO.SNOHandle(0x000354E3);
+            var actor2 = actorSNOHandle.Target as Actor;
+            var PRTransform2 = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.6130235f,
+                    Vector3D = new Vector3D(0, 0, -0.7900648f)
+                },
+                Vector3D = new Vector3D(2847.069f, 2975.214f, 24.43476f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354E3), PRTransform, world, actor.TagMap);
+            var actorID2 = WorldGenerator.loadActor(actorSNOHandle2, PRTransform2, world, actor2.TagMap);
 
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 0f;
-            // Speed.Value: 0.108
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011070;
-            translateMessage.Position = new Vector3D(2848.365f, 2975.214f, 24.69485f);
             //Send message to all players
-
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                {
+                    Angle = 0f,
+                    ActorId = (int)actorID2,
+                    AnimationTag = 0x00011070,
+                    Position = new Vector3D(2848.365f, 2975.214f, 24.69485f)
+                }, player.Value);
+
+                world.BroadcastIfRevealed(new PlayAnimationMessage
+                {
+                    ActorID = actorID2,
+                    Field1 = 3,
+                    Field2 = 0,
+                    tAnim = new Net.GS.Message.Fields.PlayAnimationMessageSpec[]
+                {
+                    new Net.GS.Message.Fields.PlayAnimationMessageSpec()
+                    {
+                        Duration = 0x00000097,
+                        AnimationSNO = 0x00002D03,
+                        PermutationIndex = 0x00000000,
+                        Speed = 1.673818f
+                    }
+                }
+
+                }, player.Value);
             }
-            //There are no other normals
-            //world.GetActorByDynamicId(actorID).Move(new Vector3D(2850.241f, 2972.256f, 24.69642f), 0);
             #endregion
             #region ZombieSkinny_Custom_A #3
             //Script code comes here
             //ActorID: 0x79660031
             //Scale 1.35
-            var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354E3);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.8882556f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.4593496f);
-            PRTransform.Vector3D = new Vector3D(2822.784f, 2956.344f, 23.94533f);
+            var actorSNOHandle3 = new Common.Types.SNO.SNOHandle(0x000354E3);
+            var actor3 = actorSNOHandle.Target as Actor;
+            var PRTransform3 = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.8882556f,
+                    Vector3D = new Vector3D(0, 0, -0.4593496f)
+                },
+                Vector3D = new Vector3D(2822.784f, 2956.344f, 23.94533f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354E3), PRTransform, world, actor.TagMap);
-
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 5.028199f;
-            // Speed.Value: 0.108
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011070;
-            translateMessage.Position = new Vector3D(2823.187f, 2955.112f, 24.04533f);
-            //Send message to all players
+            var actorID3 = WorldGenerator.loadActor(actorSNOHandle3, PRTransform3, world, actor3.TagMap);
 
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                {
+                    Angle = 5.028199f,
+                    ActorId = (int)actorID3,
+                    AnimationTag = 0x00011070,
+                    Position = new Vector3D(2823.187f, 2955.112f, 24.04533f)
+                }, player.Value);
+
+                world.BroadcastIfRevealed(new PlayAnimationMessage
+                {
+                    ActorID = actorID3,
+                    Field1 = 3,
+                    Field2 = 0,
+                    tAnim = new Net.GS.Message.Fields.PlayAnimationMessageSpec[]
+                {
+                    new Net.GS.Message.Fields.PlayAnimationMessageSpec()
+                    {
+                        Duration = 0x00000091,
+                        AnimationSNO = 0x00002D03,
+                        PermutationIndex = 0x00000000,
+                        Speed = 1.744957f
+                    }
+                }
+
+                }, player.Value);
             }
-            //There are no other normals
-            //world.GetActorByDynamicId(actorID).Move(new Vector3D(2850.241f, 2972.256f, 24.69642f), 0);
             #endregion
+
 
             #region ZombieCrawlerBarricade #1
             //ActorID: 0x798500CA  
             //Scale: 1.6
             //Script code comes here
-            var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354FF);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.826175f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.5634137f);
-            PRTransform.Vector3D = new Vector3D(2833.75f, 2978.75f, 24.07898f);
+            var actorSNOHandle4 = new Common.Types.SNO.SNOHandle(0x000354FF);
+            var actor4 = actorSNOHandle.Target as Actor;
+            var PRTransform4 = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.826175f,
+                    Vector3D = new Vector3D(0, 0, -0.5634137f)
+                },
+                Vector3D = new Vector3D(2833.75f, 2978.75f, 24.07898f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354FF), PRTransform, world, actor.TagMap);
+            var actorID4 = WorldGenerator.loadActor(actorSNOHandle4, PRTransform4, world, actor4.TagMap);
 
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 5.086197f;
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011084;
-            translateMessage.Position = new Vector3D(2833.75f, 2978.75f, 24.07898f);
             //Send message to all players
-
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                {
+                    Angle = 5.086197f,
+                    ActorId = (int)actorID4,
+                    AnimationTag = 0x00011084,
+                    Position = new Vector3D(2833.75f, 2978.75f, 24.07898f)
+                }, player.Value);
             }
 
-            world.GetActorByDynamicId(actorID).Move(new Vector3D(2839.43f, 2964.272f, 24.19856f), 5.086219f);
+            world.GetActorByDynamicId(actorID4).Move(new Vector3D(2839.43f, 2964.272f, 24.19856f), 5.086219f);
             #endregion
             #region ZombieCrawlerBarricade #2
             //ActorID: 0x798600B9   
             //Scale: 1.6
             //Script code comes here
-            var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354FF);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.7449269f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.6671461f);
-            PRTransform.Vector3D = new Vector3D(2848.75f, 2983.75f, 24.59938f);
+            var actorSNOHandle5 = new Common.Types.SNO.SNOHandle(0x000354FF);
+            var actor5 = actorSNOHandle.Target as Actor;
+            var PRTransform5 = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.7449269f,
+                    Vector3D = new Vector3D(0, 0, -0.6671461f)
+                },
+                Vector3D = new Vector3D(2848.75f, 2983.75f, 24.59938f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354FF), PRTransform, world, actor.TagMap);
+            var actorID5 = WorldGenerator.loadActor(actorSNOHandle5, PRTransform5, world, actor5.TagMap);
 
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 4.822382f;
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011084;
-            translateMessage.Position = new Vector3D(2848.75f, 2983.75f, 24.59938f);
             //Send message to all players
-
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                {
+                    Angle = 4.822382f,
+                    ActorId = (int)actorID5,
+                    AnimationTag = 0x00011084,
+                    Position = new Vector3D(2848.75f, 2983.75f, 24.59938f)
+                }, player.Value);
             }
 
-            world.GetActorByDynamicId(actorID).Move(new Vector3D(2847.27f, 2964.801f, 24.36039f), 4.634396f);
+            world.GetActorByDynamicId(actorID5).Move(new Vector3D(2847.27f, 2964.801f, 24.36039f), 4.634396f);
             #endregion
             #region ZombieCrawlerBarricade #3
             //ActorID: 0x79870031
             //Scale: 1.6
             //Script code comes here
-            var actorSNOHandle = new Common.Types.SNO.SNOHandle(0x000354FF);
-            var actor = actorSNOHandle.Target as Actor;
-            var PRTransform = new PRTransform();
-            PRTransform.Quaternion = new Quaternion();
-            PRTransform.Quaternion.W = 0.5914196f;
-            PRTransform.Quaternion.Vector3D = new Vector3D(0, 0, -0.8063639f);
-            PRTransform.Vector3D = new Vector3D(2871.25f, 2978.75f, 26.76203f);
+            var actorSNOHandle6 = new Common.Types.SNO.SNOHandle(0x000354FF);
+            var actor6 = actorSNOHandle.Target as Actor;
+            var PRTransform6 = new PRTransform()
+            {
+                Quaternion = new Quaternion()
+                {
+                    W = 0.5914196f,
+                    Vector3D = new Vector3D(0, 0, -0.8063639f)
+                },
+                Vector3D = new Vector3D(2871.25f, 2978.75f, 26.76203f)
+            };
 
-            var actorID = WorldGenerator.loadActor(new Common.Types.SNO.SNOHandle(0x000354FF), PRTransform, world, actor.TagMap);
+            var actorID6 = WorldGenerator.loadActor(actorSNOHandle6, PRTransform6, world, actor6.TagMap);
 
-            var translateMessage = new ACDTranslateNormalMessage();
-            translateMessage.Angle = 4.40722f;
-            translateMessage.ActorId = (int)actorID;
-            translateMessage.AnimationTag = 0x00011070;
-            translateMessage.Position = new Vector3D(2871.25f, 2978.75f, 26.76203f);
             //Send message to all players
-
             foreach (var player in world.Players)
             {
-                world.BroadcastIfRevealed(translateMessage, player.Value);
+                world.BroadcastIfRevealed(new ACDTranslateNormalMessage
+                {
+                    Angle = 4.40722f,
+                    ActorId = (int)actorID6,
+                    AnimationTag = 0x00011070,
+                    Position = new Vector3D(2871.25f, 2978.75f, 26.76203f)
+                }, player.Value);
             }
 
-            world.GetActorByDynamicId(actorID).Move(new Vector3D(2845.463f, 2970.245f, 24.45933f), 3.540084f);
-            #endregion
-            */
+            world.GetActorByDynamicId(actorID6).Move(new Vector3D(2845.463f, 2970.245f, 24.45933f), 3.540084f);
+            #endregion*/
         }
     }
 }
